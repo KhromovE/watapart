67b132beba92977d946fcfdc11e228ebd1899244
Egor <khromovE@gmail.com>
2018-02-09 13:23:30 +0800

    added flow to project

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,15 @@
+// @flow
+
 import React from 'react'
+import Head from 'next/head'
+
 import './index.scss'
 
 export default () => (
   <div styleName="root">
+    <Head>
+      <link rel="stylesheet" href="/_next/static/style.css" />
+    </Head>
     <p>Hello World</p>
   </div>
 )
