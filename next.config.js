0d2f2217298a004f647c369427bab60ffb3d02f9
Egor <khromovE@gmail.com>
2018-02-11 16:54:35 +0800

    removed flow and added ts

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
+const withTypescript = require('@zeit/next-typescript')
 const withSass = require('@zeit/next-sass')
+const webpack = require('webpack')
 
-module.exports = withSass({
-  cssModules: true,
-})
+module.exports = withTypescript(
+  withSass({
+    webpack: (config, { dev }) => {
+      config.plugins.push(
+        new webpack.DefinePlugin({
+          NEXT_ENV: dev ? JSON.stringify('development') : JSON.stringify('production'),
+        }),
+      )
+
+      return config
+    },
+    cssModules: true,
+  }),
+)
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,3 @@
-// @flow
-
 import React from 'react'
 import Head from 'next/head'
 
@@ -8,7 +6,7 @@ import './index.scss'
 export default () => (
   <div styleName="root">
     <Head>
-      <link rel="stylesheet" href="/_next/static/style.css" />
+      <link rel="stylesheet" href="/_next/static/style.css"/>
     </Head>
     <p>Hello World</p>
   </div>
