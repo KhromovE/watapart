8595ddfc9ad524c14206272e9d98e8ddb3a2572d
Egor <khromovE@gmail.com>
2018-02-16 17:08:16 +0800

    added open sans

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,13 @@ import * as React from 'react'
 
 declare const NEXT_ENV: string
 
+const styles: object = {
+  fontFamily: 'Open Sans, sans-serif',
+  margin: 0,
+  minHeight: '100vh',
+  padding: 0,
+}
+
 export default class MyDocument extends Document {
   public static getInitialProps({ renderPage }) {
     const { html, head, errorHtml, chunks } = renderPage()
@@ -26,8 +33,14 @@ export default class MyDocument extends Document {
         {this.addStylesheet()}
         <title>What about this apartment</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800"
+          rel="stylesheet"
+        />
       </Head>
-      <body>
+      <body
+        style={styles}
+      >
       <Main />
       <NextScript />
       </body>
