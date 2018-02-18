13b086312e4c38b8273e9e4fb9818e6f0129c509
Egor <khromovE@gmail.com>
2018-02-18 18:43:33 +0800

    some project changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react'
 
+// Layouts
 import Main from 'layouts/Main/Main'
+
+// Components
 import SearchField from 'components/SearchField/SearchField'
 
 export default () => (
